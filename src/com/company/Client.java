234b2aca26d9b8079cf7d8cd8379969c9f307081
hknyildz <ahmethakanyildiz9@gmail.com>
2021-10-20 07:35:07 +0300

    updatedv5

---
+++ src/com/company/Client.java
@@ -0,0 +1,117 @@
+package com.company;
+
+public class Client {
+
+
+
+    public int largest(int[] bars) {
+
+        int i;
+
+        int max = bars[0];
+
+        for (i = 1; i < bars.length; i++) {
+            if (bars[i] > max) {
+                max = bars[i];
+
+            }
+        }
+        return max;
+    }
+
+    public int getSecondLargest(int[] bars) {
+
+        int secondLargestIndex = 1;
+
+        int i;
+
+        int largestNumber = bars[0];
+
+        for (i = 1; i < bars.length; i++) {
+            if (bars[i] > largestNumber) {
+                largestNumber = bars[i];
+            }
+        }
+        System.out.println(largestNumber);
+        int secondLargestNumber = 0;
+
+        for (int k = 0; k < bars.length; k++) {
+            for (int j = k + 1; j < bars.length; j++) {
+                if (bars[k] > bars[j] && bars[k] < largestNumber && bars[k] > secondLargestNumber) {
+                    secondLargestNumber = bars[k];
+                    k = secondLargestIndex;
+                }
+            }
+        }
+        return secondLargestNumber;
+    }
+
+    public int getIndexOfLargest(int[] bars, int startIndex, int endIndex) {
+        int largestNumber = bars[startIndex];
+        int largestIndex = startIndex;
+
+        for (int i = startIndex; i < endIndex; i++) {
+            if (bars[i] > largestNumber) {
+                largestNumber = bars[i];
+                largestIndex = i;
+            }
+        }
+        return largestIndex;
+    }
+
+    public int getIndexOfSecondLargest(int[] bars, int startIndex, int endIndex, int indexOfLargest) {
+
+        int largestNumber = bars[0];
+        int secondLargestIndex = 0;
+
+        if (indexOfLargest == startIndex) {
+            largestNumber = bars[startIndex + 1];
+            secondLargestIndex = startIndex + 1;
+        } else {
+            largestNumber = bars[startIndex];
+            secondLargestIndex = startIndex;
+        }
+
+        for (int i = startIndex; i < endIndex; i++) {
+            if (i == indexOfLargest) {
+                continue;
+            } else if (bars[i] > largestNumber) {
+                largestNumber = bars[i];
+                secondLargestIndex = i;
+            }
+        }
+        return secondLargestIndex;
+
+    }
+
+    int water = 0;
+    public int countWater(int[] bars, int startIndex, int endIndex) {
+
+
+        int indexOfLargest = getIndexOfLargest(bars, startIndex, endIndex);
+        int indexOfSecondLargest = getIndexOfSecondLargest(bars, startIndex, endIndex, indexOfLargest);
+        System.out.println("İndex of max:" + indexOfLargest + " index of second max: " + indexOfSecondLargest);
+
+        if (indexOfLargest > indexOfSecondLargest) {
+            for (int i = indexOfLargest - 1; i > indexOfSecondLargest; i--) {
+                water += bars[indexOfSecondLargest] - bars[i];
+            }
+            water+=countWater(bars,startIndex,indexOfSecondLargest);
+
+        } else {
+            for (int i = indexOfLargest + 1; i < indexOfSecondLargest; i++) {
+                water += bars[indexOfSecondLargest] - bars[i];
+            }
+            water+=countWater(bars,startIndex,indexOfLargest);
+
+        }
+
+        return water;
+    }
+
+    public void showWater(int[] bars) {
+        int water = countWater(bars, 0, bars.length - 1);
+        System.out.println("Needed waters amount is= "+water);
+    }
+
+}
