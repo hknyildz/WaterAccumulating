8eddb24ea8feb56b2eab580873ad9b51de96df7d
hknyildz <ahmethakanyildiz9@gmail.com>
2021-10-20 07:55:13 +0300

    updatedv5fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Client client = new Client();
 
         int[] bars1 = {7, 0, 4, 2, 5, 0, 6, 4, 0, 5};
-        int[] bars2 = {4, 0, 3};
+//        int[] bars2 = {4, 0, 3};
 
 //        System.out.println("largest number: " + client.largest(bars1) + " index of it: " + client.getIndexOfLargest(bars1));
 //        System.out.println("second largest number: " + client.getSecondLargest(bars1) + " index of it: " + client.getIndexOfSecondLargest(bars1));
