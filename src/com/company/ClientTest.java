45ed2355fa56e6c081526f318131a999025cf19f
hknyildz <ahmethakanyildiz9@gmail.com>
2021-10-21 14:03:13 +0300

    updatedv8

--- src/com/company/ClientTest.java
+++ src/com/company/ClientTest.java
@@ -1,16 +1,16 @@
 package com.company;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class ClientTest {
 
     Client client = new Client();
-    int[] bars1 = {0, 1, 9, 0, 5, 7, 0, 11,11, 2, 11, 0, 6, 4, 0, 9, 9, 9, 9, 1, 1, 0};
+    int[] bars1 = {0, 1, 9, 0, 5, 7, 0, 11, 11, 2, 11, 0, 6, 4, 0, 9, 9, 9, 9, 1, 1, 0, 0};
     int[] bars2 = {6, 0, 5, 7, 0, 4, 2, 5, 0, 6, 4, 0, 5};
     int[] bars3 = {1, 0, 3, 0, 5, 0, 7, 0, 9, 0, 11, 0, 9, 0, 7, 0, 5, 0, 3, 0, 1};
-    int[] bars4 = {200,200, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
+    int[] bars4 = {200, 200, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
     int[] bars5 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int[] bars6 = {1, 13, 0, 3, 0, 5, 0, 7, 0, 9, 0, 13, 0, 9, 0, 7, 0, 5, 0, 3, 0, 1};
 
@@ -26,5 +26,27 @@ public class ClientTest {
 
     }
 
+    @Test
+    public void getIndexOfLargestNumber() {
 
+        assertEquals(client.getIndexOfLargest(bars1, 0, bars1.length - 1), 7);
+        assertEquals(client.getIndexOfLargest(bars2, 0, bars2.length - 1), 3);
+        assertEquals(client.getIndexOfLargest(bars3, 0, bars3.length - 1), 10);
+        assertEquals(client.getIndexOfLargest(bars4, 0, bars4.length - 1), 0);
+        assertEquals(client.getIndexOfLargest(bars5, 0, bars5.length - 1), 9);
+        assertEquals(client.getIndexOfLargest(bars6, 0, bars6.length - 1), 1);
+
+    }
+
+    @Test
+    public void getIndexOfSecondLargestNumber() {
+
+        assertEquals(client.getIndexOfSecondLargest(bars1, 0, bars1.length - 1, client.getIndexOfLargest(bars1, 0, bars1.length - 1)), 8);
+        assertEquals(client.getIndexOfSecondLargest(bars2, 0, bars2.length - 1, client.getIndexOfLargest(bars2, 0, bars2.length - 1)), 0);
+        assertEquals(client.getIndexOfSecondLargest(bars3, 0, bars3.length - 1, client.getIndexOfLargest(bars3, 0, bars3.length - 1)), 8);
+        assertEquals(client.getIndexOfSecondLargest(bars4, 0, bars4.length - 1, client.getIndexOfLargest(bars4, 0, bars4.length - 1)), 1);
+        assertEquals(client.getIndexOfSecondLargest(bars5, 0, bars5.length - 1, client.getIndexOfLargest(bars5, 0, bars5.length - 1)), 8);
+        assertEquals(client.getIndexOfSecondLargest(bars6, 0, bars6.length - 1, client.getIndexOfLargest(bars6, 0, bars6.length - 1)), 11);
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,9 @@ public class Main {
 
         Client client = new Client();
 
-        int[] bars1 = {0,1,9, 0, 5, 7, 0, 11, 2, 11, 0, 6, 4, 0, 9,9,9,9,1,1,0};
-        int[] bars2 = {6, 0, 5, 7, 0, 4, 2, 5, 0, 6, 4, 0, 5};
-        int[] bars3 = {1,13,0,3,0,5,0,7,0,9,0,13,0,9,0,7,0,5,0,3,0,1};
+        int[] bars1 = {7, 0, 4, 2, 5, 0, 6, 4, 0, 5};
 
-        System.out.println("Needed water is: "+client.getNeededWater(bars3));
+        System.out.println("Needed water is: " + client.getNeededWater(bars1));
     }
 
 
